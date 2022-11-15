058cab8cb2d1ccd5d0b3e52f3d3c8183e24c66d4
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-15 09:02:49 +0530

    in progress

--- lib/second_file.dart
+++ lib/second_file.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:intro_to_flutter/util.dart';
 
 class SecondFile extends StatelessWidget {
   const SecondFile({super.key});
@@ -11,10 +12,24 @@ class SecondFile extends StatelessWidget {
       child: Container(
         height: 500.0,
         width: 300.0,
-        child: Text(
-          generateRandomNumbers(),
-          textDirection: TextDirection.ltr,
-        ), 
+        alignment: Alignment(0, 0),
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
+            stops: [0.2, 0.5, 0.7, 1.0],
+            center: Alignment(0.1, 0.3),
+            focal: Alignment(-0.1, 0.6),
+          ),
+        ),
+        child: GestureDetector(
+          onTap: (() {
+            final message = SnackBar(content: Text('This is a SnackBar '));
+          }),
+          child: Text(
+            generateRandomNumbers(),
+            textDirection: TextDirection.ltr,
+          ),
+        ),
       ),
     );
   }
