a0b2909e26389192d2bc7037a91e778b4daf7a1f
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-15 11:46:34 +0530

    Text, Column Widgets

--- lib/second_file.dart
+++ lib/second_file.dart
@@ -8,32 +8,49 @@ class SecondFile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.yellow,
-      child: Container(
-        height: 500.0,
-        width: 300.0,
-        alignment: Alignment(0, 0),
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
-            stops: [0.2, 0.5, 0.7, 1.0],
-            center: Alignment(0.1, 0.3),
-            focal: Alignment(-0.1, 0.6),
-          ),
-        ),
-        child: GestureDetector(
-          onTap: (() {
-            final message = SnackBar(
-                content: const Text(
-                  'This is a SnackBar ',
-                ),
-                action: SnackBarAction(label: 'Undo', onPressed: () {}));
-            ScaffoldMessenger.of(context).showSnackBar(message);
-          }),
-          child: Text(
-            generateRandomNumbers(),
-            textDirection: TextDirection.ltr,
-          ),
+      // color: Colors.yellow,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 500.0,
+              width: 300.0,
+              color: lightPurpleColor,
+              // alignment: const Alignment(0, 0),
+              // decoration: const BoxDecoration(
+              //   gradient: RadialGradient(
+              //     colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
+              //     stops: [0.2, 0.5, 0.7, 1.0],
+              //     center: Alignment(0.1, 0.3),
+              //     focal: Alignment(-0.1, 0.6),
+              //   ),
+              // ),
+              // child: GestureDetector(
+              //   onTap: (() {
+              //     final message = SnackBar(
+              //         content: const Text(
+              //           'It was pressed',
+              //         ),
+              //         action: SnackBarAction(label: 'Undo', onPressed: () {}));
+              //     ScaffoldMessenger.of(context).showSnackBar(message);
+              //   }),
+              //   child: Text(
+              //     generateRandomNumbers(),
+              //     textDirection: TextDirection.ltr,
+              //     style: const TextStyle(
+              //         fontSize: 15.0,
+              //         color: Colors.white,
+              //         fontWeight: FontWeight.bold),
+              //   ),
+              // ),
+            ),
+            Container(
+              width: 100.0,
+              height: 500.0,
+              color: Colors.green,
+            ),
+          ],
         ),
       ),
     );
